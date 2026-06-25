b6ec78e8f84d534d5c6421181b6ef3a69c3be2f3
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2026-06-25 18:39:30 +0500

    Set version to v0.0.4

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 window.game = {
-  "version": "v0.0.3",
+  "version": "v0.0.4",
   "authServer": "https://shyfog-auth.topcatto8.workers.dev/api",
   "captchaSiteKey": "6LePli8tAAAAABxR-Y8ZfzDCQORwxLSXzbMMKHAl",
   "blockSize": 32,
