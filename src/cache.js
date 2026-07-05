23612240bb001405fcdb5ff81b86f1921cd43fc0
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2026-07-06 01:47:00 +0500

    Fix: Pre-cache destroy stages

--- src/cache.js
+++ src/cache.js
@@ -18,3 +18,15 @@ function saveTexture(file, source) {
   game.texturesCache.set(file, new Image);
   game.texturesCache.get(file).src = source;
 }
+
+// Pre-cache destroy stages
+getTexture("/block/destroy_stage_0.png");
+getTexture("/block/destroy_stage_1.png");
+getTexture("/block/destroy_stage_2.png");
+getTexture("/block/destroy_stage_3.png");
+getTexture("/block/destroy_stage_4.png");
+getTexture("/block/destroy_stage_5.png");
+getTexture("/block/destroy_stage_6.png");
+getTexture("/block/destroy_stage_7.png");
+getTexture("/block/destroy_stage_8.png");
+getTexture("/block/destroy_stage_9.png");
